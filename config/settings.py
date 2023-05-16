360e923e3ad6af36be8cc119ab114b5e5d7a2f86
Illia <illia09092003@gmail.com>
2023-05-17 01:19:41 +0300

    fixed wsgi

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8a2ken7+m6sksx@oi*g%&818$taj5&(0(b$ro-&favf872^&j6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.vercel.app']
 
